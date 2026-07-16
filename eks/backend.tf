cc00135f0c4d544afce1e76958d453f831cc1422
Pavan Kumar <106539644+Pavankumarchowdary9999@users.noreply.github.com>
2026-07-16 19:48:46 +0530

    Update S3 backend configuration for Terraform

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,11 +7,11 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
-    region         = "us-east-1"
-    key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    bucket         = "terraform-eks-state-bucket-160402"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-south-1"
     encrypt        = true
+    use_lockfile = true
   }
 }
 
